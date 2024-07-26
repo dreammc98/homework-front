dd53ba775fbeaedeb78152bc93014bd2a4d217d5
dreammc98 <dreammc98@gmail.com>
2024-07-26 12:14:03 +0700

    HW2

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -1,26 +1,35 @@
 import React from "react";
 import s from "./FriendMessage.module.css";
-import { MessagePropsType } from "../message/Message";
 
-// создать тип вместо any и отобразить приходящие данные
-const FriendMessage = (props: MessagePropsType) => {
+type FriendMessagesPropsType = {
+  message: {
+    id: number;
+    user: {
+      avatar: string;
+      name: string;
+    };
+    message: {
+      text: string;
+      time: string;
+    };
+  };
+};
+
+const FriendMessage = (props: FriendMessagesPropsType) => {
   return (
     <div id={"hw1-friend-message-" + props.message.id} className={s.friendMessage}>
       <div className={s.friendImageAndText}>
-        <img
-          id={"hw1-friend-avatar-" + props.message.user.avatar}
-          src={props.message.user.avatar}
-        />
+        <img id={"hw1-friend-avatar-" + props.message.id} src={props.message.user.avatar} />
         <div className={s.friendText}>
-          <div id={"hw1-friend-name-" + props.message.user.name} className={s.friendName}>
+          <div id={"hw1-friend-name-" + props.message.id} className={s.friendName}>
             {props.message.user.name}
           </div>
-          <pre id={"hw1-friend-text-" + props.message.message.text} className={s.friendMessageText}>
+          <pre id={"hw1-friend-text-" + props.message.id} className={s.friendMessageText}>
             {props.message.message.text}
           </pre>
         </div>
       </div>
-      <div id={"hw1-friend-time-" + props.message.message.time} className={s.friendTime}>
+      <div id={"hw1-friend-time-" + props.message.id} className={s.friendTime}>
         {props.message.message.time}
       </div>
     </div>
--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -11,17 +11,17 @@ const Message = (props: MessagePropsType) => {
   return (
     <div id={"hw1-message-" + props.message.id} className={s.message}>
       <div className={s.imageAndText}>
-        <img id={"hw1-avatar-" + props.message.user.avatar} src={props.message.user.avatar} />
+        <img id={"hw1-avatar-" + props.message.id} src={props.message.user.avatar} />
         <div className={s.text}>
-          <div id={"hw1-name-" + props.message.user.name} className={s.name}>
+          <div id={"hw1-name-" + props.message.id} className={s.name}>
             {props.message.user.name}
           </div>
-          <pre id={"hw1-text-" + props.message.message.text} className={s.messageText}>
+          <pre id={"hw1-text-" + props.message.id} className={s.messageText}>
             {props.message.message.text}
           </pre>
         </div>
       </div>
-      <div id={"hw1-time-" + props.message.message.time} className={s.time}>
+      <div id={"hw1-time-" + props.message.id} className={s.time}>
         {props.message.message.time}
       </div>
     </div>
