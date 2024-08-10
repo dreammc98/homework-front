7d82a49dbf1d2cd73162074fe9e0bf85994fafd1
dreammc98 <dreammc98@gmail.com>
2024-08-10 18:52:44 +0700

    HW07

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -1,70 +1,68 @@
-import React, {
-    ChangeEvent,
-    InputHTMLAttributes,
-    DetailedHTMLProps,
-    HTMLAttributes,
-} from 'react'
-import s from './SuperRadio.module.css'
+import React, { ChangeEvent, InputHTMLAttributes, DetailedHTMLProps, HTMLAttributes } from "react";
+import s from "./SuperRadio.module.css";
+
+/*
+ * 2 - в файле SuperRadio.tsx дописать логику функции onChangeCallback
+ * 3 - в файле SuperRadio.tsx дописать name, checked, value (узнать для чего в радио name)
+ * */
 
 type DefaultRadioPropsType = DetailedHTMLProps<
-    InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
->
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
 // тип пропсов обычного спана
-type DefaultSpanPropsType = DetailedHTMLProps<
-    HTMLAttributes<HTMLSpanElement>,
-    HTMLSpanElement
->
+type DefaultSpanPropsType = DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>;
 
-type SuperRadioPropsType = Omit<DefaultRadioPropsType, 'type'> & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
+type SuperRadioPropsType = Omit<DefaultRadioPropsType, "type"> & {
+  options?: any[];
+  onChangeOption?: (option: number) => void;
 
-    spanProps?: DefaultSpanPropsType // пропсы для спана
-}
+  spanProps?: DefaultSpanPropsType; // пропсы для спана
+};
 
 const SuperRadio: React.FC<SuperRadioPropsType> = ({
-    id,
-    name,
-    className,
-    options,
-    value,
-    onChange,
-    onChangeOption,
-    spanProps,
-    ...restProps
+  id,
+  name,
+  className,
+  options,
+  value,
+  onChange,
+  onChangeOption,
+  spanProps,
+  ...restProps
 }) => {
-    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // делают студенты
+  const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.value);
+    if (onChangeOption !== undefined) {
+      onChangeOption(Number(e.currentTarget.value));
     }
+  };
 
-    const finalRadioClassName = s.radio + (className ? ' ' + className : '')
-    const spanClassName = s.span + (spanProps?.className ? ' ' + spanProps.className : '')
+  const finalRadioClassName = s.radio + (className ? " " + className : "");
+  const spanClassName = s.span + (spanProps?.className ? " " + spanProps.className : "");
 
-    const mappedOptions: any[] = options
-        ? options.map((o) => (
-              <label key={name + '-' + o.id} className={s.label}>
-                  <input
-                      id={id + '-input-' + o.id}
-                      className={finalRadioClassName}
-                      type={'radio'}
-                      // name, checked, value делают студенты
+  const mappedOptions: any[] = options
+    ? options.map((o) => (
+        <label key={name + "-" + o.id} className={s.label}>
+          <input
+            id={id + "-input-" + o.id}
+            className={finalRadioClassName}
+            type={"radio"}
+            // name, checked, value делают студенты
+            checked={o.id === value}
+            value={o.id}
+            name={name}
+            onChange={onChangeCallback}
+            {...restProps}
+          />
+          <span id={id + "-span-" + o.id} {...spanProps} className={spanClassName}>
+            {o.value}
+          </span>
+        </label>
+      ))
+    : [];
 
-                      onChange={onChangeCallback}
-                      {...restProps}
-                  />
-                  <span
-                      id={id + '-span-' + o.id}
-                      {...spanProps}
-                      className={spanClassName}
-                  >
-                      {o.value}
-                  </span>
-              </label>
-          ))
-        : []
+  return <div className={s.options}>{mappedOptions}</div>;
+};
 
-    return <div className={s.options}>{mappedOptions}</div>
-}
-
-export default SuperRadio
+export default SuperRadio;
