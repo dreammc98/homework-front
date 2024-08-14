a060b595339857b86f3e35dc6de945a0018e1bc0
dreammc98 <dreammc98@gmail.com>
2024-08-14 21:00:30 +0700

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,34 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+/*
+ * 1 - дописать типы и логику (сортировка по имени, фильтрация по совершеннолетию) homeWorkReducer, проверить тестом
+ * */
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+const initialPeople: UserType[] = [
+  // студенты могут поменять имя/возраст/количество объектов, _id должны быть целочисленные
+  { _id: 0, name: "Кот", age: 3 },
+  { _id: 1, name: "Александр", age: 66 },
+  { _id: 2, name: "Коля", age: 16 },
+  { _id: 3, name: "Виктор", age: 44 },
+  { _id: 4, name: "Дмитрий", age: 40 },
+  { _id: 5, name: "Ирина", age: 55 },
+];
+type ActionType = { type: "sort"; payload: "up" | "down" } | { type: "check"; payload: number };
 
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      if (action.payload === "up") {
+        return [...state].sort((a, b) => (a.name > b.name ? 1 : -1));
+      } else {
+        return [...state].sort((a, b) => (a.name < b.name ? 1 : -1));
+      }
     }
-}
+    case "check": {
+      return state.filter((u) => u.age > 17);
+    }
+    default:
+      return state;
+  }
+};
