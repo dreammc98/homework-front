17eb76a389cda54878b26ea9579c182e74c11758
dreammc98 <dreammc98@gmail.com>
2024-09-11 23:57:57 +0700

    slider

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,47 @@
-import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import React from "react";
+import { Slider, SliderProps } from "@mui/material";
 
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
-}
+  return (
+    <Slider
+      sx={styles}
+      {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+    />
+  );
+};
 
-export default SuperRange
+export default SuperRange;
+
+const styles = {
+  width: "140px",
+  "& .MuiSlider-thumb": {
+    //   position: "relative",
+    backgroundColor: "transparent",
+    border: "1px solid #00CC22",
+    "&::before": {
+      content: '""',
+      // position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      width: 8,
+      height: 8,
+      borderRadius: "50%",
+      backgroundColor: " #01CB22",
+    },
+    "&:hover": {
+      boxShadow: "none",
+      backgroundColor: "transparent",
+    },
+    "&:focus-within": {
+      boxShadow: "none",
+      backgroundColor: "transparent",
+    },
+  },
+  "& .MuiSlider-track": {
+    backgroundColor: "#00CC22",
+  },
+  "& .MuiSlider-rail": {
+    backgroundColor: "#8B8B8B",
+  },
+};
